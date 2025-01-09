2a902d29f449f1664d6e5c376a5d6bec2d6501e9
VictorSouzaa0 <victor.r.souzaa05@gmail.com>
2025-01-09 05:39:33 -0300

    opa

---
+++ js/arrays.js
@@ -0,0 +1,19 @@
+//              01234567891011
+//O índice de uma string se base em cada letra
+const myName = "Victor Souza"
+// --------------------------------------------------------------------------------------------------------
+//O ídice de um array se basea em cada string dentro dele
+//                    0       1       2
+const students = ["Victor","Pedro","joao"];
+//É substituído a índice do array selecionado
+// students[1] = "Maria"; 
+// console.log(students) ['Victor', 'Maria', 'joao']
+// --------------------------------------------------------------------------------------------------------
+// console.log(students); ['Victor', 'Pedro', 'joao']
+// selecionado o índice que será  apresentado
+// console.log(students[0]);Victor
+// console.log(students[2]);Joao
+// --------------------------------------------------------------------------------------------------------
+// adcionando uma nova string no array selecionado
+// students[3] = "Otávio"
+// console.log(students)['Victor', 'Pedro', 'joao', 'Otávio']
