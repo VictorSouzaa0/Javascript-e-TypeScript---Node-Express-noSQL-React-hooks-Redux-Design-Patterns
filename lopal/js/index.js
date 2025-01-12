1461330e8f2b21862b62ea76c2077d57829ee90f
VictorSouzaa0 <victor.r.souzaa05@gmail.com>
2025-01-12 17:43:37 -0300

    lopal1

---
+++ lopal/js/index.js
@@ -0,0 +1,34 @@
+/*
+Operadores de comparação
+> : Maior que
+>= : Maior ou igual
+< : Menor que
+<= : Menor ou igual
+== : Igual a que (valor)
+==== : Igualdade estrita (valor e tipo)
+!= : O Diferente (valor)
+! == : O diferente estrito (valor e tipo)
+const num1 = 10;
+const num2 = '10'
+console.log(num1 === num2);
+*/
+
+/*
+Operadores lógicos
+
+&& -> AND -> E : Todas as expresseões precisam ser verdadeiras para retorar verdadeira
+const user = 'Victor'; //Form usuário digitou
+const time = '123456'; //Form usuário digitou
+ //              true                false
+const log = user === 'Victor' && time === '12345';
+
+console.log(log)
+|| -> OR -> OU
+const user = 'Victor'; //Form usuário digitou
+const time = '123456'; //Form usuário digitou
+ //              true                false
+const log = user === 'Victor'|| time === '12345';
+console.log(log) //o login seria efetuado caso pelo menos 1 que seja verdadeiro
+! -> NOT -> NÂO: Ele inverte qualquer valor que seja verdadeiro ou falso
+*/
+
