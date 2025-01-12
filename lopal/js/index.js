d9821767d9c2841a3805035ff132e1cb165a3d58
VictorSouzaa0 <victor.r.souzaa05@gmail.com>
2025-01-12 18:23:11 -0300

    lopal2

--- lopal/js/index.js
+++ lopal/js/index.js
@@ -32,3 +32,32 @@ console.log(log) //o login seria efetuado caso pelo menos 1 que seja verdadeiro
 ! -> NOT -> NÂO: Ele inverte qualquer valor que seja verdadeiro ou falso
 */
 
+/*
+Operadores conidionais
+IF : Pode ser usado sozinho
+Sempre utilizo a palavra else, preciso de um if antes
+Eu posso ter vários elses ifs na checagem
+Só posso ter um else na checagem
+Podemos usar condições sem else if, utilizando apenas if e else
+Entre 0 - 11 : Bom dia
+Entre 12 e 17 : Boa tarde
+Entre 18 - 23 :Boa noite
+const hr =  2024;
+
+if (hr >= 0 && hr <=11){
+    console.log('Bom dia')
+}else if (hr >= 12 && hr <=17){
+    console.log('Boa tarde')
+}else if(hr >= 18  && hr <= 23){
+    console.log('Boa noite')
+}else{
+    console.log('A hora inserida está fora do formato de 24horas')
+}
+const haveMoney = true;
+
+if(haveMoney){
+    console.log('Im out')
+}else{
+    console.log('Dont have money')
+}
+*/
