8e9ce63a516e3dd0d7169e48b4e0be601829ff34
VictorSouzaa0 <victor.r.souzaa05@gmail.com>
2025-02-19 23:20:42 -0300

    DOM exercises

---
+++ learning/js/forin.js
@@ -0,0 +1,9 @@
+const fruits = ['apple','pinneaple','grape','pear'];
+for (i = 0; i < fruits.length; i++){
+    // console.log(fruits[i])
+}
+
+for (let indice in fruits) {
+    console.log(fruits[indice])
+}
+
--- section3/js/main.js
+++ section3/js/main.js
@@ -1,7 +1,8 @@
 //Criando um array com objetos
 const elements = [
-    {tag: 'p', text: 'Word 1'},
-    {tag:'div', text: 'wword 2'},
+    {tag: 'h1', text: 'FUNCIONOU EBAAAA'},
+    {tag: 'p', text: 'word 1'},
+    {tag:'div', text: 'word 2'},
     {tag: 'footer', text: 'word 3'},
     {tag: 'section', text: 'word 4'},
 ];
@@ -10,7 +11,7 @@ const container = document.querySelector('.container');
 //cria uma const 'div' para criar a tag dentro do html
 const div =  document.createElement('div');
 
-//criando um 'for' 
+//criando um 'for':
 for (let i = 0; i < elements.length; i++) {
 //realizando a destruturação do obejto:
     let { tag,text } = elements[i];
@@ -18,7 +19,7 @@ for (let i = 0; i < elements.length; i++) {
     let createdTag = document.createElement(tag);
 //Dentro da tag criada adicionando o valor do obejto (text)
     createdTag.innerHTML = text;
-
+//adicionando as tag criadas dentro de uma div
     div.appendChild(createdTag);
     console.log(text)
 }
