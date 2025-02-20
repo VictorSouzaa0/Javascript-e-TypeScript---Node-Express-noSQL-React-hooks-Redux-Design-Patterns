c6382ccd4fe0e4ea64b44f0f7deedb38243d4730
VictorSouzaa0 <victor.r.souzaa05@gmail.com>
2025-02-20 01:44:38 -0300

    for

--- learning/js/forin.js
+++ learning/js/forin.js
@@ -1,9 +1,20 @@
-const fruits = ['apple','pinneaple','grape','pear'];
-for (i = 0; i < fruits.length; i++){
-    // console.log(fruits[i])
-}
+//                 0         1         2       3
+// const fruits = ['apple','pinneaple','grape','pear'];
+// //For in -> lêos indices ou chaves do objeto
+// for (i = 0; i < fruits.length; i++){
+//     // console.log(fruits[i])
+// }
 
-for (let indice in fruits) {
-    console.log(fruits[indice])
-}
+// for (let indice in fruits) {
+//     console.log(fruits[indice])
+// }
 
+const people =  {
+    name : 'Victor',
+    surname : 'Souza',
+    age : 20,
+};
+
+for (let x in people){
+    console.log(x,people[x])
+}
