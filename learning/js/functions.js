cd10fc148811d8fea3eaab297a3414d5a1973ffa
VictorSouzaa0 <victor.r.souzaa05@gmail.com>
2025-02-22 09:23:56 -0300

    arrays

--- learning/js/functions.js
+++ learning/js/functions.js
@@ -44,24 +44,54 @@
 // const funcio = createFunction();
 // console.dir(funcio)
 //--------------------------------CallBack--------------------------------------------------------
+// function print(num){
+//     console.log(`A oepração resultou em ${num}`)
+// }
 
-function f1(){
-    setTimeout(function(){
-        console.log('F1')
-    },1000)
+// function sum (a,b,callback){
+//     let op =  a + b;
+//     callback(op)
+// }
+
+// function mult(a,b,cb){
+//     let op = a * b
+//     cb(op)
+// }
+
+// sum(2,3,print);
+// mult(5,5,print);
+
+// function hello(name,callback){
+//     console.log(`Olá ${name} vamos aprender callback?`)
+//     callback();
+// }
+
+// function letsgo(){
+//     console.log('Então vamos começar!!')
+// }
+
+// hello('Victor',letsgo)
+
+// function login(name,callback){
+//     console.log(`Login efetuado com sucesso ${name}`)
+//     callback();
+// }
+
+// login('Victor',function(){
+//     console.log('Clique enter para continuar')
+// })
+//--------------------IIFE - Immediately invoked function expression-------------------------------------------------------------------
+function createPerson(name,surname){
+    return{
+        name,
+        surname,
+        speak:function(word){ //método
+            return `${name} está ${word}.`;
+        }
+        
+    }
 }
 
-function age(){
-    setTimeout(function(){
-        console.log(2025)
-    },2000)
-}
-
-function season(){
-    setTimeout(function(){
-        console.log('season')        
-    },500)
-}
-f1();
-age();
-season();
+const p1 = createPerson('Victor','Souza');
+console.log(p1)
+console.log(p1.speak('estudando JS'))
